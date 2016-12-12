ee26b327940259ed7a543f1011506e4d3b36babc
Boris <bce@opcoding.eu>
2016-12-12 15:12:59 +0100

    Update classes

--- src/Entity/ContextTransformer.php
+++ src/Entity/ContextTransformer.php
@@ -3,19 +3,9 @@
     namespace Fei\Service\Audit\Entity;
 
 
-    use League\Fractal\TransformerAbstract;
     use Fei\Service\Logger\Entity\ContextTransformer as LoggerContextTransformer;
 
     class ContextTransformer extends LoggerContextTransformer
     {
 
-        public function transform(Context $context)
-        {
-
-            return array(
-                'id'  => (int) $context->getId(),
-                'key' => $context->getKey(),
-                'value' => $context->getValue()
-            );
-        }
     }
--- src/Entity/NotificationTransformer.php
+++ src/Entity/NotificationTransformer.php
@@ -8,31 +8,4 @@
     class NotificationTransformer extends LoggerNotificationTransformer
     {
         
-        public function transform(Notification $notification)
-        {
-            
-            $contextItems = array();
-            
-            foreach ($notification->getContext() as $contextItem)
-            {
-                $contextItems[$contextItem->getKey()] = $contextItem->getValue();
-            }
-            
-            return array(
-                'id'          => (int) $notification->getId(),
-                'reported_at' => $notification->getReportedAt()->format(\DateTime::ISO8601),
-                'level'       => (int) $notification->getLevel(),
-                'flags'       => (int) $notification->getFlags(),
-                'namespace'   => $notification->getNamespace(),
-                'message'     => $notification->getMessage(),
-                'backtrace'   => $notification->getBackTrace(),
-                'user'        => $notification->getUser(),
-                'server'      => $notification->getServer(),
-                'command'     => $notification->getCommand(),
-                'origin'      => $notification->getOrigin(),
-                'category'    => $notification->getCategory(),
-                'env'         => $notification->getEnv(),
-                'context'     => $contextItems,
-            );
-        }
     }
